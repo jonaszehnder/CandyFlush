d78826f2d13f49b7fdffd9070f3565d7fbcd5006
Jonas Zehnder <J.Zehnder@adeon.ch>
2022-01-31 21:20:46 +0100

    rework 2d array

--- Field.java
+++ Field.java
@@ -12,17 +12,26 @@ public class Field extends Objects
     
     public Field(){
         switch(Greenfoot.getRandomNumber(5)){
-            case 0: COLOUR = Colour.RED; setImage(new GreenfootImage("red.png"));
+            case 0: COLOUR = Colour.RED;
+                    setImage(new GreenfootImage("red.png"));
                     break;
-            case 1: COLOUR = Colour.GREEN; setImage(new GreenfootImage("green.png"));
+            case 1: COLOUR = Colour.GREEN;
+                    setImage(new GreenfootImage("green.png"));
                     break;
-            case 2: COLOUR = Colour.LIME; setImage(new GreenfootImage("lime.png"));
+            case 2: COLOUR = Colour.LIME;
+                    setImage(new GreenfootImage("lime.png"));
                     break;
-            case 3: COLOUR = Colour.YELLOW; setImage(new GreenfootImage("yellow.png"));
+            case 3: COLOUR = Colour.YELLOW;
+                    setImage(new GreenfootImage("yellow.png"));
                     break;
-            case 4: COLOUR = Colour.BLUE; setImage(new GreenfootImage("blue.png"));
+            case 4: COLOUR = Colour.BLUE;
+                    setImage(new GreenfootImage("blue.png"));
                     break;
         }
         getImage().scale(getImage().getWidth()/10, getImage().getHeight()/10);
+    }
+    
+    public Colour getColour(){
+        return COLOUR;
     }
 }
